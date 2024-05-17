91af3ca7b3b36c5f16e39f5f7c7bcd315c23637e
Nichhhhhh <nichelle.kaiwen@gmail.com>
2024-05-17 14:23:36 +0800

    code

---
+++ NIchelle_FestivalFlow.js
@@ -0,0 +1,70 @@
+module.exports = {
+    name: "",
+    events: [],
+    location: "",
+
+    setName(n) {
+        this.name = n;
+    },
+
+    getName() {
+        return this.name;
+    },
+
+    setLocation(loc) {
+        this.location = loc;
+    },
+
+    getLocation() {
+        return this.location;
+    },
+
+    addEvent(event) {
+        this.events.push(event);
+        console.log(`Event "${event.name}" added to the festival.`);
+    },
+
+    removeEvent(eventName) {
+        const index = this.events.findIndex(event => event.name === eventName);
+        if (index !== -1) {
+            this.events.splice(index, 1);
+            console.log(`Event "${eventName}" removed from the festival.`);
+        } else {
+            console.log(`Event "${eventName}" not found.`);
+        }
+    },
+
+    updateEvent(eventName, newEvent) {
+        const index = this.events.findIndex(event => event.name === eventName);
+        if (index !== -1) {
+            this.events[index] = newEvent;
+            console.log(`Event "${eventName}" updated.`);
+        } else {
+            console.log(`Event "${eventName}" not found.`);
+        }
+    },
+
+    clearEvents() {
+        this.events = [];
+        console.log("All events cleared from the festival.");
+    },
+
+    listEvents() {
+        if (this.events.length === 0) {
+            console.log("No events scheduled.");
+        } else {
+            console.log("Festival Schedule:");
+            this.events.forEach((event, index) => {
+                console.log(`${index + 1}. ${event.name} - ${event.date}`);
+            });
+        }
+    }
+};
+
+// Example Event Object
+function createEvent(name, date) {
+    return { name, date };
+}
+
+// Export the createEvent function
+module.exports.createEvent = createEvent;
--- README.md
+++ README.md
@@ -7,3 +7,33 @@ In this readme file, describe how to use your node module. It could be similar t
 You can press **Ctrl+Shift+V** in this file in Visual Studio Code to see a live preview of the readme file.
 
 For some tips in formatting text in readme file, refer to https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax
+
+setName manage the name of the festival.
+setLocation and getLocation manage the festival's location.
+removeEvent removes an event by its name.
+updateEvent updates an existing event with new details.
+clearEvents removes all events from the festival.
+
+const festival = require('./festival');
+const createEvent = festival.createEvent;
+
+//Set festival name
+festival.setName('Summer Music Fest');
+console.log(`Festival Name: ${festival.getName()}`);
+
+//Create festival events
+const event1 = createEvent('Opening Ceremony, Pop Concert', '2024-06-01');
+const event2 = createEvent('Pop-rock Concert, Kpop Concert, Indie Concert', '2024-06-02');
+const event3 = createEvent('Rock Concert, Pop concert, Closing Ceremony', '2024-06-03');
+
+//Add events to the festival
+festival.addEvent(event1);
+festival.addEvent(event2);
+festival.addEvent(event3);
+
+//List all events
+festival.listEvents();
+
+// Update an event
+const updatedEvent = createEvent('Opening Ceremony, Grand Concert', '2024-06-01');
+festival.updateEvent('Opening Ceremony, Pop Concert', updatedEvent);
